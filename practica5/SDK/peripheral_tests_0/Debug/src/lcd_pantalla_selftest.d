35d91f8669cafc31bedce7f19bf7dd0c2b36cb6c
jromerf <javierfeb@outlook.com>
2021-02-17 22:06:21 +0100

    añadido sdk

---
+++ practica5/SDK/peripheral_tests_0/Debug/src/lcd_pantalla_selftest.d
@@ -0,0 +1,22 @@
+src/lcd_pantalla_selftest.d: ../src/lcd_pantalla_selftest.c \
+ ../src/lcd_pantalla.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xstatus.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xil_io.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xil_types.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/mb_interface.h
+
+../src/lcd_pantalla.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/xstatus.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/xil_io.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/xil_types.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/mb_interface.h:
---
+++ practica5/SDK/peripheral_tests_0/src/lcd_pantalla.c
@@ -0,0 +1,14 @@
+/*****************************************************************************
+* Filename:          D:\hlocal\SE\Practicas\practica5/drivers/lcd_pantalla_v1_00_a/src/lcd_pantalla.c
+* Version:           1.00.a
+* Description:       lcd_pantalla Driver Source File
+* Date:              Wed Feb 17 20:08:44 2021 (by Create and Import Peripheral Wizard)
+*****************************************************************************/
+
+
+/***************************** Include Files *******************************/
+
+#include "lcd_pantalla.h"
+
+/************************** Function Definitions ***************************/
+
