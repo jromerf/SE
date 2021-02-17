70f226010a9134b7bd6fda1afeceec3f7d72762a
jromerf <javierfeb@outlook.com>
2021-02-18 00:57:36 +0100

    añadido funcion de start y send para lcd

--- practica5/SDK/peripheral_tests_0/src/testperiph.c
+++ practica5/SDK/peripheral_tests_0/src/testperiph.c
@@ -27,6 +27,7 @@
  *
  */
 
+#include <stdbool.h>
 
 #include <stdio.h>
 #include "xparameters.h"
@@ -39,64 +40,107 @@
 #include "bram_header.h"
 #include "lcd_pantalla.h"
 
+#define lcd_screen 0xC7000000
+
+#define CLEAR_DISPLAY_CMD 0x00000001
+#define RETURN_HOME 0x00000002
+#define WRITE_CMD 0X00000200 // 10 0000 0000 escribe en el reg de datos
+#define FIRST_ROW 0x00000080 // 00 1000 0000
+#define SECOND_ROW 0x000000c0 //00 1100 0000
 
 
-#define baseaddr 0xC7000000
+void LCD_send(Xuint32 cmd){
+	Xuint32 stat ;
 
-int main() 
-{
+	xil_printf("%04X\n", cmd);
+	while((stat = LCD_PANTALLA_mWriteFIFOFull(XPAR_LCD_PANTALLA_0_BASEADDR)) == true){
+		xil_printf("%d\n", stat);
+		stat = LCD_PANTALLA_mWriteFIFOFull(XPAR_LCD_PANTALLA_0_BASEADDR);
+	}
+	LCD_PANTALLA_mWriteToFIFO(XPAR_LCD_PANTALLA_0_BASEADDR,0,cmd);
+}
 
+void LCD_start(){
+	LCD_PANTALLA_mResetWriteFIFO(lcd_screen);
+	LCD_send(CLEAR_DISPLAY_CMD);
+	LCD_send(RETURN_HOME);
+	LCD_send(WRITE_CMD);
+}
 
-   Xil_ICacheEnable();
-   Xil_DCacheEnable();
-
-   print("---Entering main---\n\r");
-
-   LCD_PANTALLA_SelfTest(baseaddr);
-
-   {
-      u32 status;
-      
-      print("\r\nRunning GpioOutputExample() for leds_gpio...\r\n");
-
-      status = GpioOutputExample(XPAR_LEDS_GPIO_DEVICE_ID,8);
-      
-      if (status == 0) {
-         print("GpioOutputExample PASSED.\r\n");
-      }
-      else {
-         print("GpioOutputExample FAILED.\r\n");
-      }
-   }
-   
-
-   {
-      int status;
-      
-      print("\r\nRunning Bram Example() for xps_bram_if_cntlr_0...\r\n");
-
-      status = BramExample(XPAR_XPS_BRAM_IF_CNTLR_0_DEVICE_ID);
-
-      if (status == 0) {
-         xil_printf("Bram Example PASSED.\r\n");
-      }
-      else {
-         print("Bram Example FAILED.\r\n");
-      }
-   }
-
-   
-   /*
-    * Peripheral SelfTest will not be run for xps_uartlite_0
-    * because it has been selected as the STDOUT device
-    */
+void LCD_begin(){
+	//Estab. modo de funcionamiento
+	LCD_PANTALLA_mWriteReg(lcd_screen,0,0x00111000);
+	//Estab. modo de entrada
+	LCD_PANTALLA_mWriteReg(lcd_screen,0,0x00000101);
+	//Inicializacion
+	LCD_PANTALLA_mWriteReg(lcd_screen,0,0x00000001);
+}
 
 
-   print("---Exiting main---\n\r");
+int main() {
 
-   Xil_DCacheDisable();
-   Xil_ICacheDisable();
+	Xil_ICacheEnable();
+	Xil_DCacheEnable();
 
-   return 0;
+	print("---Entering main---\n\r");
+/*
+	LCD_PANTALLA_SelfTest(lcd_screen);
+
+	//Estab. modo de funcionamiento
+	LCD_PANTALLA_mWriteReg(lcd_screen,0,0x00111000);
+	//Estab. modo de entrada
+	LCD_PANTALLA_mWriteReg(lcd_screen,0,0x00000101); // 01 0000 0001
+	//Inicializacion
+	LCD_PANTALLA_mWriteReg(lcd_screen,0,0x00000001);
+*/
+//las letras están en codigo ascii -> codigo ascii dec a hex
+	LCD_start();
+	LCD_send(WRITE_CMD + 'J');
+	LCD_send(WRITE_CMD + 'A');
+	LCD_send(WRITE_CMD + 'J');
+	LCD_send(WRITE_CMD + 'A');
+	LCD_send(WRITE_CMD + 'J');
+	LCD_send(WRITE_CMD + 'A');
+
+	{
+		u32 status;
+
+		print("\r\nRunning GpioOutputExample() for leds_gpio...\r\n");
+
+		status = GpioOutputExample(XPAR_LEDS_GPIO_DEVICE_ID,8);
+
+		if (status == 0) {
+			print("GpioOutputExample PASSED.\r\n");
+		}
+		else {
+			print("GpioOutputExample FAILED.\r\n");
+		}
+	}
+
+	{
+		int status;
+
+		print("\r\nRunning Bram Example() for xps_bram_if_cntlr_0...\r\n");
+
+		status = BramExample(XPAR_XPS_BRAM_IF_CNTLR_0_DEVICE_ID);
+
+		if (status == 0) {
+			xil_printf("Bram Example PASSED.\r\n");
+		} else {
+			print("Bram Example FAILED.\r\n");
+		}
+	}
+
+	/*
+	 * Peripheral SelfTest will not be run for xps_uartlite_0
+	 * because it has been selected as the STDOUT device
+	 */
+
+	print("---Exiting main---\n\r");
+
+	Xil_DCacheDisable();
+	Xil_ICacheDisable();
+
+	return 0;
 }
 
